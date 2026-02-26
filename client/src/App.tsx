53824fe397d3075a98429c1084ad9bae22e05df6
sayedlotfy <31789274+sayedlotfy@users.noreply.github.com>
2026-02-26 23:02:49 +0300

    Set default theme to dark mode for Liquid Glass
    Changed defaultTheme from 'light' to 'dark' in ThemeProvider to match the new macOS 26 Tahoe Liquid Glass dark mode aesthetic.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -23,7 +23,7 @@ function Router() {
 function App() {
   return (
     <ErrorBoundary>
-      <ThemeProvider defaultTheme="light">
+      <ThemeProvider defaultTheme="dark">
         <LanguageProvider>
           <TooltipProvider>
             <Toaster />
